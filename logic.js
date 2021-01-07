d079e2c83812fc68ce19cb324dd0e11a06fba332
Joseph Rhines <jprhines@gmail.com>
2021-01-06 18:32:09 -0600

    I created a js file and within the file I created a questions array with all the questions, answers, and choices from the quiz. I also created a var for the score

---
+++ logic.js
@@ -0,0 +1,11 @@
+var score = 0;
+var questions = [
+{q: "Who one the NBA Finals in 2020", a: "The Lakers", choices:["The Lakers", "The Timberwolves", "The Clippers", "The Suns"]},
+{q: "What NBA player has the most made threes", a: "Ray Allen", choices:["Steph Curry", "Reggie Miller", "Steven Jackson", "Ray Allen"]},
+{q: "What NBA player has the most blocks", a: "Hakeem Olajuwon", choices:["Michael Jordan", "Kobe Bryant", "Hakeem Olajuwon", "Scottie Pippen"]},
+{q: "What NBA player has the most dunks", a: "Dwight Howard", choices:["Shaq", "Shawn Kemp", "Gary Payton", "Dwight Howard"]},
+{q: "What NBA players has the most steals", a: "John Stockton", choices:["John Stockton", "Kevin Durant", "John Wall", "Jimmy Butler"]}
+    
+];
+
+
